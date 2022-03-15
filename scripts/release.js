32fd54070b03be633520e948712156c4e365412f
mawencan <1819293933@qq.com>
2022-03-15 19:55:49 +0800

    chore: 添加版本号

--- scripts/release.js
+++ scripts/release.js
@@ -1,20 +1,20 @@
 const { execSync } = require('child_process')
-// const { readJSONSync } = require('fs-extra')
+const { readJSONSync } = require('fs-extra')
 
-// const { version: oldVersion } = readJSONSync('package.json')
+const { version: oldVersion } = readJSONSync('package.json')
 
 execSync('npx bumpp', { stdio: 'inherit' })
 
-// const { version } = readJSONSync('package.json')
+const { version } = readJSONSync('package.json')
 
-// if (oldVersion === version) {
-//   console.log('canceled')
-//   process.exit()
-// }
+if (oldVersion === version) {
+  console.log('canceled')
+  process.exit()
+}
 
 execSync('npm run build:types', { stdio: 'inherit' })
 execSync('npm run update', { stdio: 'inherit' })
 execSync('git add .', { stdio: 'inherit' })
 
-execSync(`git commit -m "chore: release `, { stdio: 'inherit' })
-execSync(`git tag -a  -m `, { stdio: 'inherit' })
+execSync(`git commit -m "chore: release v${version}"`, { stdio: 'inherit' })
+execSync(`git tag -a v${version} -m "v${version}"`, { stdio: 'inherit' })
